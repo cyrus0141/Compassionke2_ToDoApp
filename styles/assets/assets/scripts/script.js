10903495a9dae1fb0cd074b76b3f4c5b66744348
Cyrus Kyalo <cyruskyalo2001@gmail.com>
2023-11-08 20:38:29 +0300

    fixing progress

---
+++ styles/assets/assets/scripts/script.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBIAwvw1tLIuEVudTdBycNPZoJ8CT7tO0s",
+    authDomain: "compassion-todo-app-7197d.firebaseapp.com",
+    databaseURL: "https://compassion-todo-app-7197d-default-rtdb.firebaseio.com",
+    projectId: "compassion-todo-app-7197d",
+    storageBucket: "compassion-todo-app-7197d.appspot.com",
+    messagingSenderId: "610981354257",
+    appId: "1:610981354257:web:7568e6944d96cc7ab8cab0",
+    measurementId: "G-HY33Z6L6QJ",
+  };
